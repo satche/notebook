5b85529106d7e3a183d30f38478b21cf2163ebeb
Satche <info@thomas-robert.ch>
2024-01-23 18:39:30 +0100

    Display tags on top of pages

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,4 +1,14 @@
-import DefaultTheme from 'vitepress/theme'
-import './variables.css'
+import { h } from "vue";
+import DefaultTheme from "vitepress/theme";
+import "./variables.css";
+import tags from "./tags.vue";
 
-export default DefaultTheme
+export default {
+  // https://vitepress.dev/guide/extending-default-theme
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      "doc-before": () => h(tags),
+    });
+  },
+};
---
+++ .vitepress/theme/tags.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="test">
+    <span v-for="(tag, index) in $frontmatter.tags" :key="index">
+      <Badge>{{ tag }}</Badge>
+    </span>
+    <slot></slot>
+  </div>
+</template>
